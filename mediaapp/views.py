9772b6cb2463bb0b864a2adbbf205a9dd9cbec2e
unkhan87 <ukhan87@icloud.com>
2022-12-05 01:45:38 +0000

    Initial view structure in place

--- mediaapp/views.py
+++ mediaapp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import BookAppointmentModel
 
 # Create your views here.
+
+class Home(generic.TemplateView):
+    '''
+    Displays Home Page
+    '''
+    template_name = 'index.html'
+
+class BookAppointment(CreateView):
+    '''
+    After submitting the form the user will be redirected
+    to the manage my bookings page where the request
+    will be shown as pending approval
+    '''
+
+class ManageAppointments(generic.ListView):
+    '''
+    Get the data from the database and displays it
+    to the user.
+    '''
+
+class DeleteAppoinment(DeleteView):
+    '''
+    enable users to delete already booked appointment,
+    redirects users to manage booking page
+    '''
+
+class EditAppointment(UpdateView):
+    '''
+    allow users to make changes to the appointment,
+    once completed the user get redirected to the 
+    manage bookings page
+    '''
