a13f69c6ecc68a96e87029f38fe9be91c9d36934
unkhan87 <ukhan87@icloud.com>
2022-12-05 00:21:42 +0000

    Admin panel phase 2 initial commit

--- mediaapp/admin.py
+++ mediaapp/admin.py
@@ -2,3 +2,20 @@ from django.contrib import admin
 from .models import BookAppointmentModel
 
 @admin.register(BookAppointmentModel)
+class AppointmentAdmin(admin.ModelAdmin):
+    '''
+    customising admin page so its easier for the
+    admin to manage appointments
+    '''
+    list_filter = (
+        'status', 'appointment_date', 'appointment_time', 
+        'admin_decision')
+    list_display = (
+        'title', 'name', 'company_name', 'email', 'created_date', 'appointment_date', 
+        'appointment_time', 'status')
+    search_fields = ['name']
+    actions = ['approve_appointment']
+
+    def approve_appointment(self, request, queryset):
+        queryset.update(approved=True)
+
