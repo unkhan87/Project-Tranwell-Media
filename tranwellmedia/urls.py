7bf175966573222affc0cc6d08cf027be7432a1e
unkhan87 <ukhan87@icloud.com>
2022-12-07 00:41:43 +0000

    install, integrate allauth and customise login page

--- tranwellmedia/urls.py
+++ tranwellmedia/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mediaapp.urls'), name='home'),
+    path('accounts/', include('allauth.urls')),
 ]
